73bdab1dee8bbe2b9aa0b282d3d1883bdb6b5b6e
Vladimir Rech <rechvladimir@hotmail.com>
2022-08-24 16:50:01 -0300

    Adding DELETE request.

--- requests-curl.sh
+++ requests-curl.sh
@@ -11,5 +11,7 @@ curl -d "userId=321&name=Vlad&city=Transilvania" -X POST http://localhost:5000/u
 
 # put location
 curl -d '{ "id": 1010, "title": "Python API test2", "notes": "Implementing a SQL Server database based Python API-2", "due_date": "2022-08-24" }' -H 'Content-Type: application/json' -X PUT http://localhost:5000/tasks -v
+
 # delete
-curl -d "userId=321" -X DELETE http://localhost:5000/users
+curl -d '{ "id": 1012 }' -H 'Content-Type: application/json' -X DELETE http://localhost:5000/tasks -v
+
