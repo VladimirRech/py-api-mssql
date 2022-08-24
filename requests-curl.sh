2b5471eb3ee157f602a978fc15b68221de34bd76
Vladimir Rech <rechvladimir@hotmail.com>
2022-08-24 11:28:14 -0300

    Changing POST request.

--- requests-curl.sh
+++ requests-curl.sh
@@ -4,7 +4,8 @@ curl http://127.0.0.1:5000/tasks
 # post new user
 
 # JSON
-curl -d '{ "userId": "322", "name": "Wayne", "city": "Gothan" }' -H 'Content-Type: application/json' -X POST http://127.0.0.1:5000/users
+curl -d '{ "title": "Python API test", "notes": "Implementing a SQL Server database based Python API", "due_date": "2022-08-23" }' -H 'Content-Type: application/json' -X POST http://127.0.0.1:5000/tasks -v
+
 # WITH PARAMETERS
 curl -d "userId=321&name=Vlad&city=Transilvania" -X POST http://localhost:5000/users
 
